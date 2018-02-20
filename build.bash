daadcf84ecd78b0280e61e0cc5f87f3589f5a1fc
Raphael Zimmermann <mail@raphael.li>
2018-02-20 13:24:53 +0100

    use docker...

--- build.bash
+++ build.bash
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir "export/"
-tectonic "foo.tex"
-mv "foo.pdf" "export/foo.pdf"
+# Setup tectonic command
+mkdir -p ~/.cache/docker-tectonic
+tectonic="docker run -ti --rm --user $(id -u) --volume $(pwd):/tectonic:z fabianhauser/tectonic:0.1.6-2"
+
+# Setup asciidoc command (TODO)
+asciidoctor_pdf="docker run -ti --rm --user $(id -u) --volume $(pwd):/documents:z asciidoctor/docker-asciidoctor asciidoctor-pdf"
+
+mkdir -p export/
+$tectonic foo.tex
+$asciidoctor_pdf demo.adoc
+mv ./*.pdf "export/"
